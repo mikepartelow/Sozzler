e007c86323f88aaa03838dc48c56c458ca589a9f
mike <osiris@jillybean.local>
2015-04-23 08:14:28 -0700

    canned data improvement

--- Sozzler/Recipe.swift
+++ Sozzler/Recipe.swift
@@ -52,16 +52,29 @@ class Recipe: NSManagedObject {
         var r = Recipe.create("disgusting artichoke", withRating: 5, withText: "not as bad as it sounds", inContext: context)
         Component.create(1, quantity_d: 2, unit: oz, ingredient: artichoke, recipe: r, context: context)
         Component.create(1, quantity_d: 1, unit: oz, ingredient: limeJuice, recipe: r, context: context)
+        r.component_count = 2
         
         r = Recipe.create("disgusting asparagus", withRating: 4, withText: "worse than it sounds", inContext: context)
         Component.create(2, quantity_d: 1, unit: oz, ingredient: asparagus, recipe: r, context: context)
         Component.create(1, quantity_d: 4, unit: tsp, ingredient: limeJuice, recipe: r, context: context)
+        r.component_count = 2
 
-        r = Recipe.create("disgusting rutabaga", withRating: 3, withText: "only marginally worse than it sounds", inContext: context)
+        let text = "\n".join(map((0..<30), { "long recipe text \($0)" }))
+        r = Recipe.create("disgusting rutabaga", withRating: 3, withText: text, inContext: context)
         Component.create(1, quantity_d: 3, unit: oz, ingredient: asparagus, recipe: r, context: context)
         Component.create(1, quantity_d: 1, unit: oz, ingredient: limeJuice, recipe: r, context: context)
         Component.create(1, quantity_d: 1, unit: tsp, ingredient: lemonJuice, recipe: r, context: context)
+        r.component_count = 3
         
-        context.save(nil)
+        artichoke.recipe_count = 1
+        asparagus.recipe_count = 2
+        limeJuice.recipe_count = 3
+        lemonJuice.recipe_count = 1
+        
+        var error: NSError?
+        if context.save(&error) {            
+        } else {
+            NSLog("\(error)")
+        }
     }
 }
