b8ff4e4c2a64751ecf947231691664d69ad2eca3
osiris <osiris@dreamqueen.local>
2015-04-26 10:36:04 -0700

    web import!

--- Sozzler/DataViewController.swift
+++ Sozzler/DataViewController.swift
@@ -10,10 +10,31 @@ class DataViewController: UIViewController {
     }
     
     @IBAction func onImportFromWeb(sender: UIButton) {
-        // FIXME: ask user whether to MERGE or REPLACE existing recipes.
-        //        how to deal with dups?
-        //        
-        //        for now, warn and REPLACE
+        var alert = UIAlertController(title: "", message: "Imported recipes will replace all existing recipes.", preferredStyle: .Alert)
+        
+        let doitAction = UIAlertAction(title: "Do it", style: .Default) { (action: UIAlertAction!) -> Void in
+            CoreDataHelper.factoryReset()
+            
+            // FIXME: need an alert to select URL
+            // FIXME: need a spinner while the import does its thing
+            
+            UrlRecipeSource(url: NSURL(string: "http://sainttoad.com/muddler/recipes.json")!, completion: { (recipes) -> () in
+                // FIXME: handle errors
+                NSLog("\(recipes?.count)")
+                CoreDataHelper.save(nil)
+                
+                // FIXME: tell user it worked
+                
+                // FIXME: tell controllers that they must reload()
+            }).fetch()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Forget it", style: .Default) { (action: UIAlertAction!) -> Void in }
+        
+        alert.addAction(doitAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
     }
     
     @IBAction func onImportCanned(sender: UIButton) {
--- Sozzler/RecipeSource.swift
+++ Sozzler/RecipeSource.swift
@@ -3,13 +3,9 @@ import Foundation
 import UIKit
 import CoreData
 
-protocol RecipeSource {
-    func fetch() -> [Recipe]
-}
-
-class CannedRecipeSource: RecipeSource {
+class CannedRecipeSource {
     func fetch() -> [Recipe] {
-        let recipesDict = [
+        let recipeDicts = [
             [
                 "name"          : "Disgusting Artichoke",
                 "rating"        : 5,
@@ -50,102 +46,39 @@ class CannedRecipeSource: RecipeSource {
         
         // FIXME: error reporting not just filtering
         //
-        return filter(map(recipesDict, { Recipe.create($0) }), { $0 != nil }).map { $0! }
+        return filter(map(recipeDicts, { Recipe.create($0) }), { $0 != nil }).map { $0! }
     }
 }
 
-//
-//class RecipeImporter: NSObject, NSURLConnectionDelegate {
-//    private var recipesJsonData = NSMutableData()
-//    private var finished: ((success: Bool) -> ())?
-//    
-//    private let context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext!
-//    
-//    override init() {}
-//    
-//    func injectCannedRecipes() {
-//        let recipes = [
-//            [
-//                "name"          : "Disgusting Artichoke",
-//                "rating"        : 5,
-//                "notes"         : "really not as bad as it sounds",
-//                "components"    : [
-//                    [
-//                        "quantity"      : "1/2",
-//                        "measure"       : "oz",
-//                        "ingredient"    : "artichoke"
-//                    ],
-//                    [
-//                        "quantity"      : "2",
-//                        "measure"       : "oz",
-//                        "ingredient"    : "disgusting"
-//                    ]
-//                ]
-//            ],
-//            
-//            [
-//                "name"          : "Disgusting Asparagus",
-//                "rating"        : 4,
-//                "notes"         : "worse than it sounds",
-//                "components"    : [
-//                    [
-//                        "quantity"      : "1",
-//                        "measure"       : "oz",
-//                        "ingredient"    : "asparagus"
-//                    ],
-//                    [
-//                        "quantity"      : "1/2",
-//                        "measure"       : "oz",
-//                        "ingredient"    : "lemon juice"
-//                    ]
-//                ]
-//            ]
-//            
-//        ]
-//        
-//        saveRecipes(recipes)
-//    }
-//    
-//    func fetch(url: String,
-//        started: () -> (),
-//        finished: (success: Bool) -> ()) {
-//            
-//            self.finished = finished
-//            
-//            started()
-//            
-//            let request = NSURLRequest(URL: NSURL(string: url)!)
-//            let connection = NSURLConnection(request: request, delegate: self, startImmediately: true)!
-//    }
-//    
-//    func connection(connection: NSURLConnection!, didReceiveData data: NSData!){
-//        recipesJsonData.appendData(data)
-//    }
-//    
-//    func connectionDidFinishLoading(connection: NSURLConnection!) {
-//        if let recipes = NSJSONSerialization.JSONObjectWithData(recipesJsonData, options: nil, error: nil) as? [NSDictionary] {
-//            saveRecipes(recipes)
-//        }
-//    }
-//    
-//    func saveRecipes(recipes: [NSDictionary]) {
-//        for recipeDict in recipes {
-//            Recipe.fromDictionary(recipeDict, context: context)
-//        }
-//        
-//        var success = false
-//        var error: NSError?
-//        
-//        if context.save(&error) {
-//            success = true
-//        } else {
-//            // FIXME: unacceptable
-//            println("Could not save \(error), \(error?.userInfo)")
-//        }
-//        
-//        if let f = finished {
-//            f(success: success)
-//        }
-//    }
-//    
-//}
+class UrlRecipeSource: NSObject, NSURLConnectionDelegate {
+    private var recipesJsonData = NSMutableData()
+    private let completion: (recipes: [Recipe]?) -> ()
+
+    private let url: NSURL
+    
+    init(url: NSURL, completion: (recipes: [Recipe]?) -> ()) {
+        self.url = url
+        self.completion = completion
+
+        super.init()
+    }
+    
+    func fetch() {
+        let request = NSURLRequest(URL: url)
+        NSURLConnection(request: request, delegate: self, startImmediately: true)
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        recipesJsonData.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        var recipes: [Recipe]? = nil
+        
+        if let recipeDicts = NSJSONSerialization.JSONObjectWithData(recipesJsonData, options: nil, error: nil) as? [NSDictionary] {
+            recipes = filter(map(recipeDicts, { Recipe.create($0) }), { $0 != nil }).map { $0! }
+        }
+        
+        completion(recipes: recipes)
+    }
+}
