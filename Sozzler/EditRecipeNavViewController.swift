a34441ddcbd19a54ef4059d6b6fa04942ff44f59
mike <osiris@jillybean.local>
2015-05-04 16:36:23 -0700

    skelly

---
+++ Sozzler/EditRecipeNavViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class EditRecipeNavViewController: UINavigationController {
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // FIXME: https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIViewController_Class/index.html
+        // A view controller is the sole owner of its view and any subviews it creates. It is responsible for creating those views and for relinquishing ownership of them at the appropriate times
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = mainStoryboard.instantiateViewControllerWithIdentifier("AddRecipe") as! AddRecipeViewController
+        
+//        viewController.navigationItem.leftBarButtonItem = nil
+//        viewController.navigationItem.rightBarButtonItem = nil
+        
+        pushViewController(viewController, animated: false)
+    }
+}
